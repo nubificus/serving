35800580fd325a7b753992a6fa0c4e4a0dcea885
Ioannis Plakas <iplakas@nubificus.co.uk>
2023-12-20 11:59:18 +0200

    Remove REDIRECT_IP changes
    Signed-off-by: Ioannis Plakas <iplakas@nubificus.co.uk>

--- pkg/reconciler/revision/resources/queue.go
+++ pkg/reconciler/revision/resources/queue.go
@@ -20,7 +20,6 @@ import (
 	"fmt"
 	"math"
 	"strconv"
-	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -232,21 +231,21 @@ func makeQueueContainer(rev *v1.Revision, cfg *config.Config) (*corev1.Container
 
 	userPort := getUserPort(rev)
 	//Get REDIRECT_IP env from yaml
-	env_list := rev.Spec.GetContainer().Env
-	var redirect_ip string
-	for _, red_envVar := range env_list {
-		if red_envVar.Name == "REDIRECT_IP" {
-			redirect_ip = red_envVar.Value
-			break
-		}
-	}
+	// env_list := rev.Spec.GetContainer().Env
+	// var redirect_ip string
+	// for _, red_envVar := range env_list {
+	// 	if red_envVar.Name == "REDIRECT_IP" {
+	// 		redirect_ip = red_envVar.Value
+	// 		break
+	// 	}
+	// }
 
 	//Check RedirectIP
-	trimmed_redirect_ip := strings.TrimSpace(redirect_ip)
-	if trimmed_redirect_ip == "" {
-		//if empty set to 127.0.0.1
-		redirect_ip = "127.0.0.1"
-	}
+	// trimmed_redirect_ip := strings.TrimSpace(redirect_ip)
+	// if trimmed_redirect_ip == "" {
+	// 	//if empty set to 127.0.0.1
+	// 	redirect_ip = "127.0.0.1"
+	// }
 
 	var loggingLevel string
 	if ll, ok := cfg.Logging.LoggingLevel["queueproxy"]; ok {
@@ -381,10 +380,10 @@ func makeQueueContainer(rev *v1.Revision, cfg *config.Config) (*corev1.Container
 			Value: loggingLevel,
 		},
 
-			{
-				Name:  "REDIRECT_IP",
-				Value: redirect_ip,
-			},
+			// {
+			// 	Name:  "REDIRECT_IP",
+			// 	Value: redirect_ip,
+			// },
 
 			{
 				Name:  "SERVING_REQUEST_LOG_TEMPLATE",
